04e14b805ffec3125541a528a31b9ee285c09431
Elton Manoel <eltonmds@usp.br>
2016-08-31 11:28:22 -0300

    add EXPRESS

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
+var express = require('express');
 var http = require('http');
 
 var ip = "localhost";
 var porta = "3000";
 
 http.createServer(function (req, res){
+          console.log(req);
                       res.writeHead(200, {'content-type': 'text/plain'});
                       res.end('Hello World\n');
 }).listen(porta, ip);
